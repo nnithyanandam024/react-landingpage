9cb1818b84a644ba296ca376a9581e49d9f84c19
nnithyanandam24 <nnithyanandam24@gmail.com>
2025-02-11 12:37:45 +0530

    Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <h1>About Us</h1>
+      <h1 className="">About Us</h1>
       <p>Welcome to our website. We are dedicated to providing the best service possible.</p>
       <p>Our team is composed of experienced professionals who are passionate about what they do.</p>
       <p>Thank you for visiting our site. We hope you find what you're looking for.</p>
